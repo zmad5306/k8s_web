00b8c649f243329ea0152f73044c944119846e51
zmad5306 <zachary.maddox@gmail.com>
2018-03-16 12:38:51 -0400

    added api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'app';
+  list = null;
+
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit() {
+    this.http.get('/api/list-service/list').subscribe((data) => {
+      this.list = data;
+    });
+  }
+
 }
